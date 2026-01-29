88109af7b27872893e8f46254f1569395245cd71
DANIEL-ANDREWS-ho <daniel.andrews3@homeoffice.gov.uk>
2026-01-29 23:22:00 +0000

    Remove all provider configuration from the module to block depends_on from module calls

--- modules/msk-provisioned-ecs-producer/image.tf
+++ modules/msk-provisioned-ecs-producer/image.tf
@@ -1,11 +1,3 @@
-provider "docker" {
-  registry_auth {
-    address  = data.aws_ecr_repository.repo.repository_url
-    username = data.aws_ecr_authorization_token.token.user_name
-    password = data.aws_ecr_authorization_token.token.password
-  }
-}
-
 resource "docker_image" "producer" {
   name         = local.image_name
   keep_locally = false
