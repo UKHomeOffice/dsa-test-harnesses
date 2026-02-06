7a3a9d8f1089a33bd1d089e358cc9f37626371a8
DANIEL-ANDREWS-ho <daniel.andrews3@homeoffice.gov.uk>
2026-02-06 09:02:40 +0000

    Update conditions and formatting

--- modules/lambda-s3-writer/locals.tf
+++ modules/lambda-s3-writer/locals.tf
@@ -6,7 +6,7 @@ locals {
   azs = length(var.availability_zone_names) == 3 ? var.availability_zone_names : slice(data.aws_availability_zones.available.names, 0, 3)
 
   # schedule enabled if non-null and non-empty
-  schedule_enabled = var.schedule_expression != null && trimspace(var.schedule_expression) != ""
+  schedule_enabled = try(trimspace(var.schedule_expression), "") != ""
 
   # Packaging selection
   is_image = var.package_type == "Image"
@@ -16,6 +16,11 @@ locals {
   use_local_zip = var.package_mode == "local_existing_zip"
   use_s3_zip    = var.package_mode == "s3_existing_zip"
 
-  lambda_filename         = local.use_archive ? data.archive_file.lambda_zip[0].output_path : (local.use_local_zip ? var.local_existing_package_path : null)
-  lambda_source_code_hash = local.use_archive ? data.archive_file.lambda_zip[0].output_base64sha256 : (local.use_local_zip ? filebase64sha256(var.local_existing_package_path) : null)
+  lambda_filename = local.is_zip && local.use_archive ? try(data.archive_file.lambda_zip[0].output_path, null) : (
+    local.is_zip && local.use_local_zip ? var.local_existing_package_path : null
+  )
+
+  lambda_source_code_hash = local.is_zip && local.use_archive ? try(data.archive_file.lambda_zip[0].output_base64sha256, null) : (
+    local.is_zip && local.use_local_zip ? filebase64sha256(var.local_existing_package_path) : null
+  )
 }
